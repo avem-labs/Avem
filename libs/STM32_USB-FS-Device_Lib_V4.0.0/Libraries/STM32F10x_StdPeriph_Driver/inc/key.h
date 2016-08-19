1e2ae45fbc9bd2e1d633908672e63973b614aeb8
but0n <but0n@icloud.com>
2016-08-19 18:55:45 +0800

    Edited Pause

--- libs/STM32_USB-FS-Device_Lib_V4.0.0/Libraries/STM32F10x_StdPeriph_Driver/inc/key.h
+++ libs/STM32_USB-FS-Device_Lib_V4.0.0/Libraries/STM32F10x_StdPeriph_Driver/inc/key.h
@@ -3,7 +3,7 @@
 #define _KEY_H
 void delay(volatile unsigned int count);
 #define KEY1 BIT_ADDR((GPIOC_BASE+8), 13)
-#define PAUSE() {while(KEY1);delay(200);}
+#define PAUSE() {while(KEY1);delay(10);}
 
 void Key_init();
 
