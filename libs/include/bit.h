31242a50cbc534e9287be22c15b1deb748a0650e
but0n <but0n@icloud.com>
2017-05-02 14:20:27 +0800

    feat: (bit band) A little bit enhancement

--- libs/include/bit.h
+++ libs/include/bit.h
@@ -1,7 +1,7 @@
 #ifndef BIT_H
 #define BIT_H
 
-#define BITBAND(addr, bitnum) ((addr & 0xF0000000) + 0x2000000 + ((addr & 0xFFFFF) << 5) + (bitnum<<2))
-#define BIT_ADDR(addr, bitnum) *((volatile unsigned long *)(BITBAND(addr, bitnum)))
+#define BITBAND(addr, bitnum) (((addr) & 0xF0000000) + 0x2000000 + (((addr) & 0xFFFFF) << 5) + ((bitnum)<<2))
+#define BIT_ADDR(addr, bitnum) *((volatile unsigned long *)(BITBAND((unsigned int)(addr), bitnum)))
 
 #endif
